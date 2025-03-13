7dabb306e08d119742dc0e74920361a07b330a00
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-13 15:52:05 +0100

    edit routes

--- routers/posts.js
+++ routers/posts.js
@@ -2,32 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 //index
-route.get("/posts" , (req,res) => {
+route.get("/" , (req,res) => {
     res.send("Lista dei post");
 });
 
 //show
-route.get("posts/:id", (req, res) => {
+route.get("/:id", (req, res) => {
     res.send(`Dettagli del post con id: ${postId}`);
 });
 
 //store
-route.post("posts/", (req, res) => {
+route.post("/", (req, res) => {
     res.send(`Creazione nuovo post`);
 });
 
 //update
-route.put("posts/:id", (req, res) => {
+route.put("/:id", (req, res) => {
     res.send(`Modifica totale del post con id: ${postId}`);
 });
 
 //modify
-route.patch("posts/:id", (req, res) => {
+route.patch("/:id", (req, res) => {
     res.send(`Modifica parziale del post con id: ${postId}`);
 });
 
 //delete
-route.delete("posts/:id", (req, res) => {
+route.delete("/:id", (req, res) => {
     res.send(`Eliminazione del post con id: ${postId}`);
 });
 
